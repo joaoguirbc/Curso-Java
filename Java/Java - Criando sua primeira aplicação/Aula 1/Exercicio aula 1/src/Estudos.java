43b3b5d30deb06360ae8fd5edf00b55d065ded5e
João Guilherme Cavalcanti <joaogui76@gmail.com>
2024-08-29 12:15:33 -0300

    Primeiro commit - Inicialização do repositório

---
+++ Java/Java - Criando sua primeira aplicação/Aula 1/Exercicio aula 1/src/Estudos.java
@@ -0,0 +1,8 @@
+public class Estudos {
+    public static void main(String[] args){
+        System.out.println("A linguagem Java tem as seguintes características:");
+        System.out.println("Fortemente tipada");
+        System.out.println("Orientada à objetos");
+        System.out.println("Alto nível");
+    }
+}
